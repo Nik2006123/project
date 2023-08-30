f37223101fe531a52ec5af1e07031a61454e6834
Nik2006123 <141873498+Nik2006123@users.noreply.github.com>
2023-08-30 09:21:55 -0400

    Add files via upload

---
+++ homwork/hw.py
@@ -0,0 +1,10 @@
+country = "Sakartvelo" 
+
+print (country[3:5])
+print(country[1:11])
+print(country[0:5])
+print(country[2:5])
+
+print(country[3:5]) + (country[0:5])
+print(country[1:11]) + (country[3:5])
+print(country[2:5]) + (country[1:1])
