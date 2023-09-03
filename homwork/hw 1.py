a31a446a6d341eed2c7295aed505b59abba992dc
Nik2006123 <141873498+Nik2006123@users.noreply.github.com>
2023-09-03 12:27:04 -0400

    Add files via upload

---
+++ homwork/hw 1.py
@@ -0,0 +1,51 @@
+from turtle import *
+#step 1:  draw a square
+speed(100)
+def კვადრატის_დახაზვა():
+    for i in range(4):
+        forward(200)
+        left(90)
+კვადრატის_დახაზვა()
+#end of square
+#drawing a door()
+forward(70)
+left(90)
+forward(100)   #height of the door
+right(90)
+forward(60)
+right(90)
+forward(100)
+#end of door
+#draw a triangle
+penup()
+goto(200, 200)
+pendown()
+
+right(150)
+forward(200)
+left(120)
+forward(200)
+#end of triangle
+#start a 1 windows
+penup()
+goto(15, 170)
+pendown()
+left(30)
+forward(50)
+def window():
+    for i in range(3):
+        left(90)
+        forward(50)
+window()
+#end of first window
+#start second window
+penup()
+goto(180, 170)
+pendown()
+def window_2():
+    for i in range(5):
+        forward(50)
+        left(90)
+window_2()
+#end of house
+exitonclick()
---
+++ homwork/hw 3.py
@@ -0,0 +1,9 @@
+def triangle_perimeter(გვერდი_1, გვერდი_2, გვერდი_3):
+    perimeter = გვერდი_1 + გვერდი_2 + გვერდი_3
+    print("Triangle Perimeter:", perimeter)
+
+# Example usage
+გვერდი_1 = float(input("Enter the length of the first side: "))
+გვერდი_2= float(input("Enter the length of the second side: "))
+გვერდი_3 = float(input("Enter the length of the third side: "))
+triangle_perimeter(გვერდი_1, გვერდი_2, გვერდი_3)
